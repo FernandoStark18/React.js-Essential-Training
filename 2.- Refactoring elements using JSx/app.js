11b3e482530f4cb2e09147ce4081453cd526ab45
Fernando Espericueta <fernandostark18@gmail.com>
2023-08-15 12:27:48 -0600

    Fixing Unexpected token '<' error using Babel

--- 2.- Refactoring elements using JSx/app.js
+++ 2.- Refactoring elements using JSx/app.js
@@ -1,11 +1,12 @@
 ReactDOM.render(
-    <ul>
-        <li>Monday</li>
-        <li>Tuesday</li>
-        <li>Wednesday</li>
-        <li>Thursday</li>
-        <li>Friday</li>
-        <li>Saturday</li>
-        <li>Sunday</li>
-    <ul/>, 
-    document.getElementById("root"));
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+    <li>Thursday</li>
+    <li>Friday</li>
+    <li>Saturday</li>
+    <li>Sunday</li>
+  </ul>,
+  document.getElementById("root")
+);
