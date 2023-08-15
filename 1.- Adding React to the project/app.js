2bffda89979d878c89551cce026d5fc26eb6e321
Fernando Espericueta <fernandostark18@gmail.com>
2023-08-15 12:02:18 -0600

    Code before refactoring

--- 1.- Adding React to the project/app.js
+++
@@ -1,3 +0,0 @@
-let heading = React.createElement("h1", null, "Getting Started with React");
-
-ReactDOM.render(heading, document.getElementById("root"));
---
+++ 2.- Refactoring elements using JSx/app.js
@@ -0,0 +1,13 @@
+let heading = React.createElement(
+  "ul",
+  null,
+  React.createElement("li", null, "Monday"),
+  React.createElement("li", null, "Tuesday"),
+  React.createElement("li", null, "Wednesday"),
+  React.createElement("li", null, "Thursday"),
+  React.createElement("li", null, "Friday"),
+  React.createElement("li", null, "Saturday"),
+  React.createElement("li", null, "Sunday")
+);
+
+ReactDOM.render(heading, document.getElementById("root"));
