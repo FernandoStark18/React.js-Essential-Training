9d0ae5f5b9ab520d5d21642f1446ce4d4ce53528
Fernando Espericueta <fernandostark18@gmail.com>
2023-09-01 11:26:38 -0600

    Working with useEffect

--- 8.- Generating a project with Create React App/react-app/src/App.js
+++ 8.- Generating a project with Create React App/react-app/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App({ library }) {
   const [emotion, setEmotion] = useState("happy");
-  console.log();
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, [emotion]); // The brackets alone means that the effect is not going to be called again after the first render. It is called the DEPENDENCY ARRAY because we can use it to define when this should be called. Should it just be called one when our component first renders or another option is we could pass in a property a state value to listen for any changes in this array
+
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
