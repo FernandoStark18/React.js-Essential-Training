c55d1f4fbbeb995ad7b2d90193c37a9ad28c05fa
Fernando Espericueta <fernandostark18@gmail.com>
2023-09-03 16:50:58 -0600

    Displaying data from an API

--- 8.- Generating a project with Create React App/react-app/src/App.js
+++ 8.- Generating a project with Create React App/react-app/src/App.js
@@ -1,6 +1,16 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function GithubUSer({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} />
+    </div>
+  );
+}
+
 function App() {
   const [data, setData] = useState(null);
   useEffect(() => {
@@ -9,7 +19,14 @@ function App() {
       .then(setData);
   }, []);
 
-  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  if (data)
+    return (
+      <GithubUSer
+        name={data.name}
+        location={data.location}
+        avatar={data.avatar_url}
+      />
+    );
   return <h1>{data}</h1>;
 }
 
