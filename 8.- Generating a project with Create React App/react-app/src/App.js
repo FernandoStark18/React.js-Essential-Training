fe7e23f86d40dbec736e45fb200a224ae35e5ca0
Fernando Espericueta <fernandostark18@gmail.com>
2023-09-03 16:38:57 -0600

    Fetching data with Hooks

--- 8.- Generating a project with Create React App/react-app/src/App.js
+++ 8.- Generating a project with Create React App/react-app/src/App.js
@@ -1,33 +1,16 @@
 import "./App.css";
-import { useState } from "react";
-
-function useInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-    {
-      value,
-      onChange: (e) => setValue(e.target.value),
-    },
-    () => setValue(initialValue),
-  ];
-}
+import { useState, useEffect } from "react";
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value}, ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type="text" aceholder="color tittle..."></input>
-      <input {...colorProps} type="color"></input>
-      <button>ADD</button>
-    </form>
-  );
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("https://api.github.com/users/FernandoStark18")
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  if (data) return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <h1>{data}</h1>;
 }
 
 export default App;
